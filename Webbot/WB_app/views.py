94c6a7779adb32462ca65462b9321406bd0794b6
root <fedeguastadisegni@gmail.com>
2016-02-09 11:54:03 -0300

    API lista. Solr Arreglado

--- Webbot/WB_app/views.py
+++ Webbot/WB_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from urllib2 import *
 import urllib
 
+
 def get_id(request,id_solicitud):
     print id_solicitud
 
@@ -21,7 +22,7 @@ def get_list(request):
 def get_autor(request,nombre):
     print nombre
     
-    url = 'http://localhost:8983/solr/Index/select?q=Autor:"'+urllib.quote(nombre)+'"&wt=json'
+    url = 'http://localhost:8983/solr/Index/select?q=Autor:"'+urllib.quote(nombre)+'"&wt=json&rows=10000'
     conn = urlopen(url)
     rsp = eval( conn.read() )
 
@@ -30,7 +31,34 @@ def get_autor(request,nombre):
 def get_titulo(request,titulo):
     print titulo
     
-    url = 'http://localhost:8983/solr/Index/select?q=Titulo:"'+urllib.quote(titulo)+'"&wt=json'
+    url = 'http://localhost:8983/solr/Index/select?q=Titulo:"'+urllib.quote(titulo)+'"&wt=json&rows=10000'
+    conn = urlopen(url)
+    rsp = eval( conn.read() )
+
+    return JsonResponse(rsp['response'])
+
+def get_url(request,url):
+    print url
+    
+    url = 'http://localhost:8983/solr/Index/select?q=URL:"'+urllib.quote(url)+'"&wt=json&rows=10000'
+    conn = urlopen(url)
+    rsp = eval( conn.read() )
+
+    return JsonResponse(rsp['response'])
+
+def get_isbn(request,isbn):
+    print isbn
+    
+    url = 'http://localhost:8983/solr/Index/select?q=ISBN:"'+urllib.quote(isbn)+'"&wt=json&rows=10000'
+    conn = urlopen(url)
+    rsp = eval( conn.read() )
+
+    return JsonResponse(rsp['response'])
+
+def get_anio(request,anio):
+    print anio
+    
+    url = 'http://localhost:8983/solr/Index/select?q=Anio:"'+urllib.quote(anio)+'"&wt=json&rows=10000'
     conn = urlopen(url)
     rsp = eval( conn.read() )
 
